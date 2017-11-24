6e806556a1c1abe3cf9480d531434967cc5a0069
Gabriel Xavier <gabrielx4000@hotmail.com>
2017-11-24 16:36:29 -0200

    applying the same model used for the sequential version

--- knn-parallel.py
+++ knn-parallel.py
@@ -10,11 +10,12 @@ predictions = {}
 predictions_index = 0
 thread_number = 0
 test_set_size = 0
+data_set_columns_size = 0
 wrong_predictions = 0
-
+neighbors_number = 0
 
 """
-  Loads iris dataset (.csv) and randomly divide dataset into training and tests
+  Loads dataset (.csv) and randomly divide dataset into training and tests
   sets in order to perform the knn algorithm
 """
 def load_data(filename, ratio, training_set=[], test_set=[]):
@@ -22,16 +23,16 @@ def load_data(filename, ratio, training_set=[], test_set=[]):
     lines = csv.reader(csvfile)
     dataset = list(lines)
     for x in range(len(dataset)-1):
-      for y in range(9):
+      for y in range(data_set_columns_size):
         dataset[x][y] = float(dataset[x][y])
         if random.random() < ratio:
           training_set.append(dataset[x])
         else:
           test_set.append(dataset[x])
 
-""" 
-  Calculates the euclidean distance between two given arrays 
-  <first> and <second> of length <size> 
+"""
+  Calculates the euclidean distance between two given arrays
+  <first> and <second> of length <size>
 """
 def euclidean_distance(first, second, size):
   distance = 0
@@ -40,7 +41,7 @@ def euclidean_distance(first, second, size):
 
   return math.sqrt(distance)
 
-""" 
+"""
   Get <k> neighbors of <test_obj> analyzing the <training_set>
 """
 def get_neighbors(training_set, test_obj, k):
@@ -56,7 +57,7 @@ def get_neighbors(training_set, test_obj, k):
 	  neighbors.append(distances[x][0])
   return neighbors
 
-""" 
+"""
   Extracts the appropriate class according to the neighbors
 """
 def get_response(neighbors):
@@ -70,19 +71,8 @@ def get_response(neighbors):
   sorted_votes = sorted(class_votes.items(), key=operator.itemgetter(1), reverse=True)
   return sorted_votes[0][0]
 
-""" 
-  Gets the accuracy of our knn algorithm
-"""
-def get_accuracy(test_set):
-  correct = 0
-  for x in range(len(test_set)):
-    if test_set[predictions[x][0]][-1] == predictions[x][1]:
-      correct += 1
-  return ((correct/float(len(test_set))) * 100.0)
-
 def make_prediction(testValue, training_set):
-  k = 3
-  neighbors = get_neighbors(training_set, testValue, k)
+  neighbors = get_neighbors(training_set, testValue, neighbors_number)
   return get_response(neighbors)
 
 def print_result(predicted, actual, pos):
@@ -90,11 +80,11 @@ def print_result(predicted, actual, pos):
   maxSpacingSize = len(repr(test_set_size))
   actualSpacingSize = len(repr(pos))
   for i in range(maxSpacingSize - actualSpacingSize):
-    space = space + '0'  	
+    space = space + '0'
   if repr(predicted) == repr(actual):
     print(space + repr(pos) + '> predicted=' + repr(predicted) + ', actual=' + repr(actual))
-  else: 
-    print(space + bcolors.WARNING + repr(pos) + '> predicted=' + repr(predicted) + ', actual=' + repr(actual) + bcolors.ENDC)	
+  else:
+    print(space + bcolors.WARNING + repr(pos) + '> predicted=' + repr(predicted) + ', actual=' + repr(actual) + bcolors.ENDC)
 
 def parallel_knn(test_set,training_set, current_thread, test_set_size):
   # parallelization divide logic
@@ -113,48 +103,54 @@ def parallel_knn(test_set,training_set, current_thread, test_set_size):
     if repr(result) != repr(test_set[index][-1]):
 	  global wrong_predictions
 	  wrong_predictions += 1
-    
+
     #print_result(result, test_set[index][-1], index+1)
     index += 1
-  
-  #wrong_predictions = wrong
-  
+
 def main():
-	
+
   # checkin command line arguments
-  if(len(sys.argv) != 2):
-	  print(bcolors.FAIL + 'Invalid command line arguments.\nMust be: <program_name> <thread_count>' + bcolors.ENDC)
+  if(len(sys.argv) != 6):
+	  print(bcolors.FAIL + 'Invalid command line arguments.\nMust be: <program name> <dataset name> <columns size> <ratio> <number of neighbors> <thread count>' + bcolors.ENDC)
 	  sys.exit()
-	
-  global thread_number
-  thread_number = int(float(sys.argv[1]))
-  threadList = []
-  	
+
   # prepare data
+  dataset_name = sys.argv[1]
+  global data_set_columns_size
+  data_set_columns_size = int(float(sys.argv[2]))
+  ratio = float(sys.argv[3])
+
+  global neighbors_number
+  neighbors_number = int(float(sys.argv[4]))
+
+  global thread_number
+  thread_number = int(float(sys.argv[5]))
+
+  threadList = []
   training_set = []
   test_set = []
-  ratio = 0.2
-  load_data('breast-cancer-wisconsin.csv', ratio, training_set, test_set)
+
+  load_data(dataset_name, ratio, training_set, test_set)
+
   global test_set_size
   test_set_size = len(test_set)
-  threadList = [] 
-  
+
   start = time.time()
   for i in range(thread_number):
     thread = Thread(target=parallel_knn,args=(test_set, training_set, i, test_set_size))
     thread.start()
     threadList.append(thread)
-  for thread in threadList:  
-    thread.join()	    
+  for thread in threadList:
+    thread.join()
   end = time.time()
 
-  #accuracy = get_accuracy(test_set)
-  
-  print('\nTrain set: ' + repr(len(training_set)))
+  accuracy = ((test_set_size - wrong_predictions)/float(test_set_size)) * 100
+
+  print(bcolors.WARNING + 'Train set: ' + repr(len(training_set)))
   print('Test set: ' + repr(test_set_size))
   print('Wrong Predictions: ' + repr(wrong_predictions))
   print('Correct Predictions: ' + repr(test_set_size - wrong_predictions))
-  #print('Accuracy: ' + repr(accuracy) + '%')
-  print('Elapsed Time: ' + repr(end-start))
-	
+  print('Accuracy: ' + repr(accuracy) + '%')
+  print('Elapsed Time: ' + repr(end-start) + bcolors.ENDC)
+
 main()
